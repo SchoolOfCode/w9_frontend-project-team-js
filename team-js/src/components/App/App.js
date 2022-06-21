2d8e8c5ad48d54541d4dd6b09003511e2d119df8
Steven Cheung <stevencheung@hotmail.co.uk>
2022-06-21 11:58:53 +0100

    completed css for profile-container

--- team-js/src/components/App/App.js
+++ team-js/src/components/App/App.js
@@ -1,12 +1,19 @@
-import Profile from '../Profile';
-import './App.css';
+import Profile from "../Profile";
+import "./App.css";
 
+const dummyProfile = [
+  {
+    userName: "Adam",
+    bio: "Bootcamper no.1455",
+    imageSrc:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/therock-253954517-279993034023432-628278073878740852-n-1636731864.jpg",
+    help: ["css", "html", "react"],
+    improve: ["node.js", "postgres"],
+  },
+];
 
 function App() {
-  return (
-
-  <Profile/>
-  );
+  return <Profile profileDetails={dummyProfile} />;
 }
 
 export default App;
--- team-js/src/components/Profile/index.js
+++ team-js/src/components/Profile/index.js
@@ -1,25 +1,41 @@
 import "./index.css";
 
-function Profile() {
+function Profile({ profileDetails }) {
+  // function that takes array and adds separator ', '
+
   return (
-
     <div className="profile-container">
+      <div className="profile-pic">
+        <img
+          src={profileDetails[0].imageSrc}
+          alt="user"
+          className="profile-image"
+        />
+      </div>
 
-        <div className="profile-pic">
-            <img alt="user" className="profile-image"/>
+      <div className="info-area">
+        <div className="info-area-box">
+          <label>Username</label>
+          <p>{profileDetails[0].userName} </p>
         </div>
 
-        <div className="info-area">
-                <p>Username</p>
-                <p>Bio</p>
-                <p>I want to help with: </p>
-                <p>I want to improve: </p>
+        <div className="info-area-box">
+          <label>Bio</label>
+          <p>{profileDetails[0].bio}</p>
         </div>
 
+        <div className="info-area-box">
+          <label>I want to help with</label>
+          <p>{profileDetails[0].help} </p>
+        </div>
 
+        <div className="info-area-box">
+          <label>I want to improve</label>
+          <p>{profileDetails[0].improve} </p>
+        </div>
+      </div>
     </div>
-  )
-  
+  );
 }
 
 export default Profile;

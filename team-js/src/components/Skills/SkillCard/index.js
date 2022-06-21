da8f2ca01b1326b01e7368a1afef38647f4cf9e7
Steven Cheung <stevencheung@hotmail.co.uk>
2022-06-21 16:43:58 +0100

    added css for skillCard

--- team-js/src/components/Skills/SkillCard/index.js
+++ team-js/src/components/Skills/SkillCard/index.js
@@ -7,19 +7,21 @@ function SkillCard({ id, title, starRating, callChangeRating, notes }) {
   return (
     <div className="Skill-card" id={id} {...getToggleProps()}>
       <label>{title}</label>
-      <p {...getCollapseProps()}>{notes}</p>;
-      <Ratings
-        rating={starRating}
-        widgetDimensions="2vw"
-        widgetSpacings="0vw"
-        changeRating={callChangeRating}
-      >
-        <Ratings.Widget widgetRatedColor="yellow" widgetHoverColor="yellow" />
-        <Ratings.Widget widgetRatedColor="yellow" widgetHoverColor="yellow" />
-        <Ratings.Widget widgetRatedColor="yellow" widgetHoverColor="yellow" />
-        <Ratings.Widget widgetRatedColor="yellow" widgetHoverColor="yellow" />
-        <Ratings.Widget widgetRatedColor="yellow" widgetHoverColor="yellow" />
-      </Ratings>
+      <p {...getCollapseProps()}>{notes}</p>
+      <div className="star-container">
+        <Ratings
+          rating={starRating}
+          widgetDimensions="2vw"
+          widgetSpacings="0vw"
+          changeRating={callChangeRating}
+        >
+          <Ratings.Widget widgetRatedColor="yellow" widgetHoverColor="yellow" />
+          <Ratings.Widget widgetRatedColor="yellow" widgetHoverColor="yellow" />
+          <Ratings.Widget widgetRatedColor="yellow" widgetHoverColor="yellow" />
+          <Ratings.Widget widgetRatedColor="yellow" widgetHoverColor="yellow" />
+          <Ratings.Widget widgetRatedColor="yellow" widgetHoverColor="yellow" />
+        </Ratings>
+      </div>
     </div>
   );
 }

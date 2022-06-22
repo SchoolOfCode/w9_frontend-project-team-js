f322da0a9723df75c4a9cd178166d562afa8e578
Adam P <adam.phasey@hotmail.com>
2022-06-22 11:44:12 +0100

    Set up JSX

---
+++ team-js/src/components/Header/index.js
@@ -0,0 +1,20 @@
+import './index.css'
+
+
+function Header () {
+
+    return (
+            
+        <header className='header1'>
+
+        <h1></h1>
+
+        
+        </header>
+       
+
+    )
+
+}
+
+export default Header

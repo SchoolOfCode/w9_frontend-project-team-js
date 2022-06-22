638dfc556f094b3c9badaf4a2268915353d95a40
ClaudiaGC1339 <claudiagonzalez1339@gmail.com>
2022-06-22 16:38:36 +0100

    added menu button and menu items however needs to fix positioning

--- team-js/src/components/App/App.js
+++ team-js/src/components/App/App.js
@@ -54,12 +54,6 @@ function App() {
     <div>
     
     <header className='Header-top'>
-    <Menu customBurgerIcon={ <img src="https://img.icons8.com/external-justicon-lineal-color-justicon/344/external-pencil-case-back-to-school-justicon-lineal-color-justicon.png"/> }>
-        <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/about">About</a>
-        <a id="contact" className="menu-item" href="/contact">Contact</a>
-
-      </Menu>
     <Header />
     </header>
     <div className="app-container">

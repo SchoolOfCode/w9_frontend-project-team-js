2cc624414c4b771fa19d801eb4c39b760a494147
ClaudiaGC1339 <claudiagonzalez1339@gmail.com>
2022-06-29 13:38:07 +0100

    added test for goal input

--- team-js/src/components/Goals/GoalInput/goalinput.test.js
+++ team-js/src/components/Goals/GoalInput/goalinput.test.js
@@ -1,3 +1,11 @@
 import { test, expect, jest } from "@jest/globals";
 import { fireEvent, render, screen } from "@testing-library/react";
 import Input from "./index.js";
+
+test("renders an <input> in document", () => {
+    render(<Input buttonText="test" addGoal={jest.fn()}/>);
+
+    const tag = screen.getByRole("textbox");
+    console.log(tag);
+    expect(tag).toBeInTheDocument();
+});

f74a54415b6249af1b8d23debaf0c434b9e53639
skandog <muumrar@gmail.com>
2022-06-29 15:07:50 +0100

    written suite of tests for goalinput, could do with some more, but agaisnt time decided to move onto other components in goal tree

--- team-js/src/components/Goals/GoalInput/goalinput.test.js
+++ team-js/src/components/Goals/GoalInput/goalinput.test.js
@@ -1,11 +1,30 @@
 import { test, expect, jest } from "@jest/globals";
 import { fireEvent, render, screen } from "@testing-library/react";
+// import { userEvent } from "@testing-library/user-event";
 import Input from "./index.js";
 
 test("renders an <input> in document", () => {
-    render(<Input buttonText="test" addGoal={jest.fn()}/>);
+  render(<Input buttonText="test" addGoal={jest.fn()} />);
 
-    const tag = screen.getByRole("textbox");
-    console.log(tag);
-    expect(tag).toBeInTheDocument();
+  const tag = screen.getByRole("textbox");
+  expect(tag).toBeInTheDocument();
+});
+
+test("renders button that calls function onClick", () => {
+  //const user = userEvent.setup();
+
+  render(<Input addGoal={jest.fn()} onClick={jest.fn()} />);
+
+  const tag = screen.getByRole("button");
+  const event = fireEvent.click(tag);
+  expect(event).toBeTruthy();
+});
+
+test("whether text can be typed into input field", () => {
+  render(<Input buttonText="test" addGoal={jest.fn()} />);
+  const tag = screen.getByRole("textbox");
+  const typed = fireEvent.keyDown(tag);
+
+  console.dir(typed);
+  expect(typed).toBeTruthy();
 });
